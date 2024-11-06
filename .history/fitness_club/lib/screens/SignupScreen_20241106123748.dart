9a698daf7c759782cb0f89efb4328e70dd2c8b9b
unknown <meroo1995saggaf@outlook.com>
2024-11-06 12:39:54 +0300

    add Django backend and some flutter files :laptop:

---
+++ .history/fitness_club/lib/screens/SignupScreen_20241106123748.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Signupscreen extends StatefulWidget {
+  const Signupscreen({super.key});
+
+  @override
+  State<Signupscreen> createState() => _SignupscreenState();
+}
+
+class _SignupscreenState extends State<Signupscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Signup page"),
+    );
+  }
+}
